345003404515844d99eb5e85735ef99f5e3f3672
azza9885 <49562774+azza9885@users.noreply.github.com>
2020-01-05 02:43:58 -0500

    Created Basket service with GetBasket,Add Basket,Additemto basket,remove item from Basket

---
+++ AzMyeStore/AzMyeStore.Services/BasketService.cs
@@ -0,0 +1,134 @@
+﻿using AzMyeStore.Core.Contracts;
+using AzMyeStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AzMyeStore.Services
+{
+    public class BasketService
+    {
+        // the workflow that is being accomplished here : read the cookie from the user and with in that cookie 
+        // look for the basket ID and if it found load the basket, if it is not found return an empty inmemory basket
+        //in order to read cookies we use something called httpcontext, this is basically the method of connecting user 
+        //to the server and it contains a lot of information clients automatically send when you connect to a web server
+        // such as IP address and one other thing that is also included is a list of cookies.Because AzMyestore.Services 
+        //has no clue what httpcontext is we need to add the relevant libraries in Reference libraries for services project.
+        //System.Web.
+        //now our basket service needs access to underlying data and for this we need to access IRepository
+
+        IRepository<Product> productContext;
+        IRepository<Basket> basketContext;
+        public const string BasketsessionName = "eCommerceBasket";
+        //this const string cannot be updated elsewhere in our code
+        //this is not neccessarily required however when we are reading and writing a cookie we use a string to identify a 
+        // a particular cookie to enforce consistency whenever we read and write to/from a cookie
+        public BasketService(IRepository<Product> ProductContext,IRepository<Basket> BasketContext)
+        {
+            this.productContext = ProductContext;
+            this.basketContext = BasketContext;
+        }
+
+        //now we can go ahead and write our methods, first one would be to load the user basket ,
+        // read the users cookie from the httpcontext looking for the basket ID and look for that 
+        //basket ID in the database
+        
+        private Basket GetBasket(HttpContextBase httpContext,bool createifNull)
+        //this  is an internal method which we will use from various places within our service
+        // because we want have access to user's httpcontext we will force the consuming service 
+        //to send this httpContext as part of the call and we use the boolean variable because sometimes
+        // we would want to create the basket if none exists and sometimes we dont.
+        {
+            HttpCookie cookie = httpContext.Request.Cookies.Get(BasketsessionName);
+            //finding and reading a cookie
+
+            Basket basket = new Basket();
+
+            if(cookie!=null)
+            {
+                string basketId = cookie.Value;
+                if (!String.IsNullOrEmpty(basketId))
+                {
+                    basket = basketContext.Find(basketId);
+                }
+                
+                else
+                {
+                    if(createifNull)
+                    {
+                        basket = CreateNewBasket(httpContext);
+                    }
+
+                }
+            }
+
+            else
+            {
+                if (createifNull)
+                {
+                    basket = CreateNewBasket(httpContext);
+                }
+            }
+
+            return basket;
+        }
+        
+        private Basket CreateNewBasket(HttpContextBase httpContext)
+            // this method is also private since we are only going to use internally with in the basket service class
+        {
+            Basket basket = new Basket();
+            basketContext.Insert(basket);
+            basketContext.Commit();
+
+            HttpCookie cookie = new HttpCookie(BasketsessionName);
+            cookie.Value = basket.Id;
+            cookie.Expires = DateTime.Now.AddDays(7);
+            httpContext.Response.Cookies.Add(cookie);
+            //finally we have to assign the cookie to the httpcontext response, because it has to be sent back to the user.
+
+            return basket;
+        }
+       
+        public void AddtoBasket(HttpContextBase httpContext,string productId)
+        {
+            Basket basket = GetBasket(httpContext, true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.ProductId == productId);
+
+            if (item == null)
+            {
+                item = new BasketItem()
+                {
+                    BasketId = basket.Id,
+                    ProductId = productId,
+                    Quantity = 1
+
+                };
+                basket.BasketItems.Add(item);
+            }
+
+            else
+            {
+                item.Quantity = item.Quantity + 1;
+            }
+
+            basketContext.Commit();
+        }
+
+        public void RemoveFromBasket(HttpContextBase httpContext, string itemId)
+        {
+            Basket basket = GetBasket(httpContext, true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.Id == itemId);
+
+            if(item!= null)
+            {
+                basket.BasketItems.Remove(item);
+                basketContext.Commit()
+            }
+
+            
+        }
+    }
+};
